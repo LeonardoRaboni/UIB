f37a830dcceeb5043196f4119efe7aa4aba8fb16
Leonardo Lucena Raboni <1192305679@acd.grupotiradentes.com>
2020-02-10 19:19:17 -0300

    pedindo nome do usuário

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner leTeclado = new Scanner (System.in);
+		
+		
+		System.out.println ("Informe seu nome");
+		
+		String nome = leTeclado.next(); 
+		
+		System.out.println ("Hello, " + nome + "!");
+		
+		
 	}
 
 }
